b5f481aadefc0f269cd2c8a15d72b8987a2be022
KOALA\renpu <rairaicindylou@gmail.com>
2024-04-02 13:18:14 +0800

    added new data which is user

--- index.js
+++ index.js
@@ -2,26 +2,47 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-//routes
-app.get('/pets', (req, res) => {
-    const pets = [
-        {
-            id: 1,
-            name: "Daschund",
-        },
-        {
-            id: 2,
-            name: "Corgi",
-        },
-        {
-            id: 3,
-            name: "Chihuahua-ua",
-        },
-    ];
+//Pet data
+const pets = [
+    {
+        id: 1,
+        name: "Daschund",
+    },
+    {
+        id: 2,
+        name: "Corgi",
+    },
+    {
+        id: 3,
+        name: "Chihuahua",
+    }
+];
 
+//username and password data
+const user = [
+    {
+        username: "Cindy",
+        password: "041402cindy",
+    },
+    {
+        username: "raicin",
+        password: "raisins",
+    },
+    {
+        username: "Lou",
+        password: "cindylouwho"
+    }
+];
+
+//route1 for pets
+app.get('/pets', (req, res) => {
     res.json(pets);
 });
 
+//route2 for username and pass
+app.get('/user', (req, res) => {
+    res.json(user);
+});
 
 //start
 app.listen(port, () => {
